d4ac635842a0e08e320ce582f349f6ec0b42be43
justabeginner10 <adityaraj60303@gmail.com>
2025-10-26 14:22:47 +0530

    Initial Commit

---
+++ NotesApp/NotesAppApp.swift
@@ -0,0 +1,21 @@
+//
+//  NotesAppApp.swift
+//  NotesApp
+//
+//  Created by Aditya Raj on 26/10/25.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct NotesAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
